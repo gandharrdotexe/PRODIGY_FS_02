7ada228e3a39ed20c703827e194bb740480bdbc6
gandharrdotexe <gandharbagde@gmail.com>
2024-07-24 23:02:47 +0530

    Added create and ReaD functionality. Table Pagination to be added

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/users');
+const multer = require('multer');
+
+//image upload
+var storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, './uploads');
+    },
+    filename: function(req, file, callback){
+        callback(null, file.fieldname+"_"+Date.now()+'_'+file.originalname);
+    }
+});
+
+var upload  = multer({
+    storage: storage
+}).single('image'); 
 
 router.get('/', (req, res)=>{
     // res.send('Users Page');
-    res.render('index',{title: 'Home Page'});
+    res.render('index',{title: 'Home Page', message : req.session.message});
+    delete req.session.message;
 });
 
+
+//insert an use rin databse
+router.post('/add', upload, async (req, res) => {
+    try {
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            image: req.file.filename,
+        });
+        await user.save();
+        req.session.message = {
+            type: 'success',
+            message: 'User added successfully'
+        };
+        console.log(req.session.message);
+        res.redirect('/');
+    } catch (err) {
+        res.json({ message: err.message, type: 'danger' });
+    }
+});
 router.get('/add', (req,res)=>{
     res.render('add_users', {title: 'Add Users'})
 })
